5d7409f0c61163fe635699a7dd8b6cb7259f1fa0
izskat <nicholas_scott_campos@dlsu.edu.ph>
2021-09-17 04:37:58 +0800

    To Do List done

--- app/src/main/java/com/mobdeve/s14/pomogochi/OnDialogCloseListener.java
+++
@@ -1,9 +0,0 @@
-package com.mobdeve.s14.pomogochi;
-
-import android.content.DialogInterface;
-
-public interface OnDialogCloseListener {
-
-    void onDialogClose(DialogInterface dialogInterface);
-}
-
--- app/src/main/java/com/mobdeve/s14/pomogochi/TodoListActivity.java
+++ app/src/main/java/com/mobdeve/s14/pomogochi/TodoListActivity.java
@@ -1,66 +1,73 @@
 package com.mobdeve.s14.pomogochi;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class TodoListActivity extends AppCompatActivity implements OnDialogCloseListener{
-    private RecyclerView mRecyclerview;
-    private FloatingActionButton fab;
-    private DataBaseHelper myDB;
-    private List<ToDoModel> mList;
-    private ToDoAdapter adapter;
-
-    private int money;
+public class TodoListActivity extends AppCompatActivity
+{
+    private ListView noteListView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_todo_list);
+        initWidgets();
+        loadFromDBToMemory();
+        setNoteAdapter();
+        setOnClickListener();
+    }
 
-        mRecyclerview = findViewById(R.id.recyclerview);
-        fab = findViewById(R.id.fab);
-        myDB = new DataBaseHelper(TodoListActivity.this);
-        mList = new ArrayList<>();
-        adapter = new ToDoAdapter(myDB, TodoListActivity.this);
 
-        mRecyclerview.setHasFixedSize(true);
-        mRecyclerview.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerview.setAdapter(adapter);
+    private void initWidgets()
+    {
+        noteListView = findViewById(R.id.rvToDoList);
+    }
 
-        mList = myDB.getAllTasks();
-        Collections.reverse(mList);
-        adapter.setTasks(mList);
+    private void loadFromDBToMemory()
+    {
+        SQLiteManager sqLiteManager = SQLiteManager.instanceOfDatabase(TodoListActivity.this);
+        sqLiteManager.populateNoteListArray();
+    }
 
-        fab.setOnClickListener(new View.OnClickListener() {
+    private void setNoteAdapter()
+    {
+        NoteAdapter noteAdapter = new NoteAdapter(getApplicationContext(), Note.nonDeletedNotes());
+        noteListView.setAdapter(noteAdapter);
+    }
+
+
+    private void setOnClickListener()
+    {
+        noteListView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                AddNewTask.newInstance().show(getSupportFragmentManager() , AddNewTask.TAG);
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l)
+            {
+                Note selectedNote = (Note) noteListView.getItemAtPosition(position);
+                Intent editNoteIntent = new Intent(getApplicationContext(), NoteDetailActivity.class);
+                editNoteIntent.putExtra(Note.NOTE_EDIT_EXTRA, selectedNote.getId());
+                startActivity(editNoteIntent);
             }
         });
-//        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new RecyclerViewTouchHelper(adapter));
-//        itemTouchHelper.attachToRecyclerView(mRecyclerview);
+    }
+
+
+    public void newNote(View view)
+    {
+        Intent newNoteIntent = new Intent(TodoListActivity.this, NoteDetailActivity.class);
+        startActivity(newNoteIntent);
     }
 
     @Override
-    public void onDialogClose(DialogInterface dialogInterface) {
-        mList = myDB.getAllTasks();
-        Collections.reverse(mList);
-        adapter.setTasks(mList);
-        adapter.notifyDataSetChanged();
+    protected void onResume()
+    {
+        super.onResume();
+        setNoteAdapter();
     }
 }
